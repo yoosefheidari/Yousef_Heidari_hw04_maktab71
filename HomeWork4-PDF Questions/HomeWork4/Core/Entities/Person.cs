e45d4448e32eda83cf7d81d2d405ac62a2df841a
Yousef <yousef1371@gmail.com>
2022-04-05 22:08:38 +0430

    modify

--- HomeWork4-PDF Questions/HomeWork4/Core/Entities/Person.cs
+++ HomeWork4-PDF Questions/HomeWork4/Core/Entities/Person.cs
@@ -1,25 +1,25 @@
-﻿//public class Person
-//{
-//    public string CardNo { get; set; }
-//    public string Name { get; set; }
-//    public string Family { get; set; }
-//    public string Height { get; set; }
-//    public string Mobile { get; set; }
-//    public string Father { get; set; }
-//    public string Weight { get; set; }
-//    public string BirthDay { get; set; }
-//    public string Address { get; set; }
-//    public Person(string card,string name,string family,string height,string mobile,string father,string weight,string birth,string address)
-//    {
-//        CardNo = card;  
-//        Name = name;
-//        Family = family;
-//        Height = height;
-//        Mobile = mobile;
-//        Father = father;
-//        Weight = weight;
-//        BirthDay = birth;
-//        Address = address;
-//    }
+﻿public class Person
+{
+    public int CardNo { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public int Height { get; set; }
+    public string Mobile { get; set; }
+    public string Father { get; set; }
+    public int Weight { get; set; }
+    public DateTime BirthDay { get; set; }
+    public string Address { get; set; }
+    public Person(int card, string name, string family, int height, string mobile, string father, int weight, DateTime birth, string address)
+    {
+        CardNo = card;
+        Name = name;
+        Family = family;
+        Height = height;
+        Mobile = mobile;
+        Father = father;
+        Weight = weight;
+        BirthDay = birth;
+        Address = address;
+    }
 
-//}
+}
